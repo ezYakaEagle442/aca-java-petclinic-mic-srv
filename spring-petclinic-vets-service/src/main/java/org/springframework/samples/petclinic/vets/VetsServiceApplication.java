885a46433ad33c757a6dcb052ae89865736f7a2c
ezYakaEagle442 <stepin.cloud@outlook.com>
2022-10-27 14:14:59 +0200

    removed Env var debug traces

--- spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
+++ spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,76 +19,90 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.samples.petclinic.vets.system.VetsProperties;
 
+import java.net.*;
 import java.util.Map;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 import org.springframework.beans.factory.annotation.Value;
+// import org.apache.commons.net.telnet.TelnetClient;
 
 /**
  * @author Maciej Szarlinski
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigurationProperties(VetsProperties.class)
 public class VetsServiceApplication {
 
-        @Value("${spring.cloud.azure.keyvault.secret.endpoint}")
-        private static String kvSecretEndpoint;
-    
-        @Value("${spring.cloud.azure.keyvault.secret.property-sources[0].endpoint}")
-        private static String kvSecretPropertySourcesEndpoint;
-    
-        @Value("${spring.datasource.url}")
-        private static String url;
-    
-        @Value("${spring.cache.cache-names}")
-        private static String cacheName;
-            
-        @Value("${spring.sql.init.mode}")
-        private static String sqlInitMode;
-
-        @Value("${spring.sql.datasource.initialization-mode}")
-        private static String sqlDataSourceInitMode;
-
-        @Value("${spring.jpa.hibernate.ddl-auto}")
-        private static String jpaHibernateDdlAuto;
-
 	public static void main(String[] args) {
+	
+		/* 
+		System.out.println("Checking ALL ENV variable  : |" + "|\n");
+		System.getenv().forEach((key, value) -> {
+			System.out.println(key + ":" + value);
+		});
 
-        System.out.println("Checking ENV variables ..."+ "\n");
+		System.out.println("Checking ALL ENV Properties  : |" + "|\n");
+		System.getProperties().forEach((key, value) -> {
+			System.out.println(key + ":" + value);
+		});
+		*/
 
-                Map envMap = System.getenv();
-                for (Object key : envMap.keySet()) {
-                        System.out.println(key + " : " + envMap.get(key));
-                }
+        String systemipaddress = "";
+        try {
+            URL url_name = new URL("http://whatismyip.akamai.com");
+            BufferedReader sc = new BufferedReader(new InputStreamReader(url_name.openStream()));
+            systemipaddress = sc.readLine().trim();
+        }
+        catch (Exception e) {
+            systemipaddress = "Cannot Execute Properly";
+        }
+        System.out.println("Public IP Address: " + systemipaddress + "\n");
 
-                System.out.println("Checking ENV variable  : |" + "|\n");
 
-                System.out.println("Checking ENV variable SPRING_PROFILES_ACTIVE : |" + System.getenv("SPRING_PROFILES_ACTIVE") + "|\n");
+		// https://github.com/Azure/AKS/blob/2022-03-27/vhd-notes/aks-ubuntu/AKSUbuntu-1804/2022.03.23.txt
+		// Telnet & Netcat look installed on the AKS nodes, but not on the App container
+		/*
+		Runtime runtime = Runtime.getRuntime();
+		try {
+			Process process =runtime.exec("telnet petcliasc.mysql.database.azure.com 3306");
+			System.out.println( "SUCCESSFULLY executed Telnet");
+        }
+        catch (Exception e) {
+			System.err.println( "Cannot Execute Telnet");
+			e.printStackTrace();
+        }
 
-                System.out.println("Checking ENV variable AZURE_KEYVAULT_ENDPOINT : |" + System.getenv("AZURE_KEYVAULT_ENDPOINT") + "|\n");
-                System.out.println("Checking ENV variable AZURE_KEYVAULT_URI : |" + System.getenv("AZURE_KEYVAULT_URI") + "|\n");
+		try {
+			Process process =runtime.exec("nc -vz petcliasc.mysql.database.azure.com 3306");
+			System.out.println( "SUCCESSFULLY executed Netcat");
+        }
+        catch (Exception e) {
+			System.err.println("Cannot Execute Netcat");
+			e.printStackTrace();
+        }
+		
+		TelnetClient telnetClient = new TelnetClient();
+		try {
+			// telnetClient.connect("petcliasc.mysql.database.azure.com", 3306);
+			telnetClient.connect(System.getenv("MYSQL_SERVER_FULL_NAME"), 3306);
+			System.out.println( "SUCCESSFULLY executed TelnetClient");
+			telnetClient.disconnect();
+        }
+        catch (Exception e) {
+			System.err.println("Cannot Execute TelnetClient");
+			e.printStackTrace();
+        }
+		*/
+        // Set StatrtUp Probe
+        // https://docs.spring.io/spring-boot/docs/2.7.x/reference/htmlsingle/#features.spring-application.startup-tracking
 
-                System.out.println("Checking ENV variable spring.cloud.azure.keyvault.secret.endpoint : |" + System.getenv("SPRING_CLOUD_AZURE_KEYVAULT_SECRET_SECRET_ENDPOINT") + "|\n");
-                System.out.println("Checking ENV variable spring.cloud.azure.keyvault.secret.property-sources[0].endpoint : |" + System.getenv("SPRING_CLOUD_AZURE_KEYVAULT_SECRET_SECRET_PROPERTYSOURCES_ENDPOINT") + "|\n");
-
-                System.out.println("kvSecretEndpoint from config file: " + kvSecretEndpoint);
-                System.out.println("kvSecretPropertySourcesEndpoint from config file: " + kvSecretPropertySourcesEndpoint);
-
-                System.out.println("JDBC URL from config file: " + url);
-                System.out.println("cache name: " + cacheName);
-                System.out.println("SQL Init mode: " + sqlInitMode);
-
-                System.out.println("sqlDataSourceInitMode: " + sqlDataSourceInitMode);
-                System.out.println("jpaHibernateDdlAuto: " + jpaHibernateDdlAuto);
-
-                System.out.println("JDBC URL from config file: " + url);
-                                
-                // Set StatrtUp Probe
-                // https://docs.spring.io/spring-boot/docs/2.7.x/reference/htmlsingle/#features.spring-application.startup-tracking
-
-                SpringApplication application = new SpringApplication(VetsServiceApplication.class);
-                application.setApplicationStartup(new BufferingApplicationStartup(2048));
-                application.run(args);	
+        SpringApplication application = new SpringApplication(VetsServiceApplication.class);
+        application.setApplicationStartup(new BufferingApplicationStartup(2048));
+        application.run(args);	
 	}
 }
